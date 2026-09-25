74c7d782d9cf4ca227af1d28afb948fff7af725c
Gurleen101 <gurleenka56@gmail.com>
2026-09-25 23:54:48 +0530

    Time: 1 ms (57.07%), Space: 8.7 MB (36%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)
+        return false;
+    int n=x;
+    long long rev=0;
+    int rem;
+    while(n>0){
+    rem=n%10;
+    rev=rev*10+rem;
+    n=n/10;
+    }
+    if(rev==x)
+    return true;
+    else 
+    return false;
+
+
+
+    
+        
+    }
+};
